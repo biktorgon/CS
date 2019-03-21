e8072a2b95e071813cf2aeb879b9ff2cdee05a73
biktorgon <biktorgon@mail.ru>
2019-03-21 15:32:25 +0300

    fix length of list

--- sorts/bubble.py
+++ sorts/bubble.py
@@ -1,11 +1,11 @@
 from sorts.swap import swap
 
 
-def buble(lst):
+def bubble(lst):
     swapped = True
     while swapped:
         swapped = False
-        for i in range(1, len(lst)-1):
+        for i in range(1, len(lst)):
             if lst[i-1] > lst[i]:
                 swap(lst, i-1, i)
                 swapped = True
