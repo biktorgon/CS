e51cb40b7d6d6b4e7ce025f38d0a5ee0926b38ee
biktorgon <biktorgon@mail.ru>
2019-03-21 17:20:57 +0300

    add insertion sort module

---
+++ sorts/insertion.py
@@ -0,0 +1,10 @@
+from sorts.swap import swap
+
+
+def insertion(lst):
+    for i in range(1, len(lst)):
+        unsorted_item = i
+        sorted_item = unsorted_item - 1
+        while sorted_item > -1 and lst[sorted_item] > lst[sorted_item+1]:
+            swap(lst, sorted_item, sorted_item + 1)
+            sorted_item -= 1
